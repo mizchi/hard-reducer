016d2b3e7dd272923b5b53e363dd00e1b0592a26
Horiuchi_H <horiuchi.g@gmail.com>
2018-01-25 17:01:24 +0900

    Add test code for typescript

---
+++ test/test.ts
@@ -0,0 +1,30 @@
+import { buildActionCreator, createReducer } from '../';
+const { createAction } = buildActionCreator({ prefix: 'counter/' });
+
+const inc = createAction('inc', (val: number) => val)
+const dec = createAction('dec', (val: number) => val)
+
+inc(1) //=> { type: 'counter/inc', payload: 1 }
+
+type State = { value: number }
+
+const initialState = { value: 0 }
+
+const reducer = createReducer(initialState)
+  // Handle `(State, Payload) => State` in matched context.
+  .case(inc, (state, payload) => {
+    return {
+      value: state.value + payload
+    }
+  })
+  .case(dec, (state, payload) => {
+    const p = payload //=> number
+    return {
+      value: state.value - p
+    }
+  })
+
+// Use it
+const ret0 = reducer(initialState, inc(3))
+const ret1 = reducer(ret0, dec(1))
+
