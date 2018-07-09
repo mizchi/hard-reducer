be6151b7c8bfa90fc9778e069bc99179ddbb7fcb
mizchi <miz404@gmail.com>
2018-07-09 18:49:00 +0900

    Export AsyncActionCreator

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,28 @@
 export type Action<T> = {
-  type: string
-  payload: T
-}
+  type: string;
+  payload: T;
+};
 
 export type ActionCreator<Input, Payload = Input> = {
-  (input: Input): Action<Payload>
-  type: string
-}
+  (input: Input): Action<Payload>;
+  type: string;
+};
 
 export type Reducer<State> = {
-  (state: State, action: any): State
-  get: () => Reducer<State>
+  (state: State, action: any): State;
+  get: () => Reducer<State>;
   case<Input, Payload>(
     actionFunc: ActionCreator<Input, Payload>,
     reducer: (state: State, payload: Payload) => State
-  ): Reducer<State>
-  else(fn: (s: State, a: Action<any>) => State): Reducer<State>
-}
+  ): Reducer<State>;
+  else(fn: (s: State, a: Action<any>) => State): Reducer<State>;
+};
+
+export type AsyncAction<Input, Payload> = {
+  started: ActionCreator<Input, void>;
+  resolved: ActionCreator<Input, Payload>;
+  rejected: ActionCreator<Input, Error>;
+} & ((input: Input) => Promise<Payload>);
 
 export const buildActionCreator: (
   opt?: { prefix?: string }
@@ -24,15 +30,12 @@ export const buildActionCreator: (
   createAction<Input, Payload>(
     t?: string | void,
     fn?: (input: Input) => Payload
-  ): ActionCreator<Input, Payload>
+  ): ActionCreator<Input, Payload>;
+
   createAsyncAction<Input, Payload>(
     t: string | void,
     fn: (input: Input) => Promise<Payload>
-  ): {
-    started: ActionCreator<Input, void>
-    resolved: ActionCreator<Input, Payload>
-    rejected: ActionCreator<Input, Error>
-  } & ((input: Input) => Promise<Payload>)
-}
+  ): AsyncAction<Input, Payload>;
+};
 
-export const createReducer: <T>(t: T) => Reducer<T>
+export const createReducer: <T>(t: T) => Reducer<T>;
--- test/typescript.ts
+++ test/typescript.ts
@@ -1,73 +1,73 @@
-import { buildActionCreator, createReducer, ActionCreator } from '../'
+import { ActionCreator, buildActionCreator, createReducer, Reducer } from "../";
 const { createAction, createAsyncAction } = buildActionCreator({
-  prefix: 'counter/'
-})
+  prefix: "counter/"
+});
 
-const reset = createAction()
-const inc = createAction('inc', (val: number) => val)
-const dec = createAction('dec', (val: number) => val)
+const reset = createAction();
+const inc = createAction("inc", (val: number) => val);
+const dec = createAction("dec", (val: number) => val);
 // typed with ActionCreator
-const foo: ActionCreator<{ foo: number }> = createAction('foo')
+const foo: ActionCreator<{ foo: number }> = createAction("foo");
 
-const _type: string = inc.type
+const _type: string = inc.type;
 
-const incAsync = createAsyncAction('inc-async', async (val: number) => {
-  return val
-})
+const incAsync = createAsyncAction("inc-async", async (val: number) => {
+  return val;
+});
 
-inc(1) //=> { type: 'counter/inc', payload: 1 }
+inc(1); //=> { type: 'counter/inc', payload: 1 }
 
-type State = { value: number }
+type State = { value: number };
 
-const initialState = { value: 0 }
+const initialState: State = { value: 0 };
 
-const reducer = createReducer(initialState)
+const reducer: Reducer<State> = createReducer(initialState)
   // Handle `(State, Payload) => State` in matched context.
   .case(inc, (state, payload) => {
     return {
       value: state.value + payload
-    }
+    };
   })
   .case(dec, (state, payload) => {
-    const p = payload //=> number
+    const p = payload; //=> number
     return {
       value: state.value - p
-    }
+    };
   })
   // require redux-thunk
   .case(incAsync.started, (state, payload) => {
-    return state
+    return state;
   })
   .case(incAsync.resolved, (state, payload) => {
     // $ExpectError
     // const p: string = payload
-    return state
+    return state;
   })
   .case(incAsync.rejected, (state, error) => {
-    return state
+    return state;
   })
   .case(reset, state => {
-    return initialState
+    return initialState;
   })
   .case(foo, (state, payload) => {
     // $ExpectError
     // const pe: { foo: string } = payload
-    return initialState
+    return initialState;
   })
   // .case('nop', (state, payload) => {
   //   return state
   // })
   .else((state, action) => {
     // console.log(action)
-    return state
-  })
+    return state;
+  });
 
 // Use it
-const assert = require('assert')
+const assert = require("assert");
 
-const ret0 = reducer(initialState, inc(3))
-const ret1 = reducer(ret0, dec(1))
+const ret0 = reducer(initialState, inc(3));
+const ret1 = reducer(ret0, dec(1));
 
-assert(ret0.value === 3)
-assert(ret1.value === 2)
-assert(inc(1).type === 'counter/inc')
+assert(ret0.value === 3);
+assert(ret1.value === 2);
+assert(inc(1).type === "counter/inc");
